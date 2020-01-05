48b8361736afc6582bb1759ee343c0b1044a3be7
Ric Messier <kilroy@washere.com>
2020-01-05 15:29:02 -0700

    Added a JavaScript sample

---
+++ sample1.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.post('/form', (req, res) => {
+  const name  = req.body.name
+  const email = req.body.email
+  const age   = req.body.age
+})
